99d6dd6fbecfc4f6918ca62cf35daad3a3b4127d
Pilar <pilarnavarily@gmail.com>
2020-09-05 20:51:18 +0200

    First assignment of JavaScript and minor changes

---
+++ assignment_javascript/js/gallery.js
@@ -0,0 +1,26 @@
+function upDate(previewPic){
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+*/
+        x=document.getElementById("image");
+        x.style.backgroundImage = `url(${previewPic.src})`;
+    
+ /* 2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+*/ 
+        x.innerHTML=previewPic.alt; 
+	}
+
+	function unDo(){
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    */
+        x=document.getElementById("image");
+        x.style.backgroundImage="url('')"; 
+   /*
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+        x.innerHTML='Hover over an image below to display here';
+	}
